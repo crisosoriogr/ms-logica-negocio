6662c17b21254105bc877b52f4ac4df6de31de3a
Cristian Camilo Osorio <cristian.1701421857@ucaldas.edu.co>
2023-09-24 13:35:06 -0500

    Creacion de modelos y datasources

---
+++ src/models/cliente.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Cliente extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  PrimerNombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  segundoNombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerApellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  segundoApellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
