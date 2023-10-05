ecb723dad33a883bcb2688f4ffa5ed6c14cd813a
Cristian Camilo Osorio <cristian.1701421857@ucaldas.edu.co>
2023-10-05 13:07:39 -0500

    Creacion del controlador de adiministracion de archivos, para la carga y descarga de archivos

---
+++ src/config/configuracion.general.ts
@@ -0,0 +1,8 @@
+export namespace ConfiguracionGeneral{
+
+  export const carpetaArchivosProductos:string='../../archivos/productos';
+
+  export const campoDeProducto:string='file';
+  export const extensionesImagenes:string[]=['.PNG','.JPG','.JPEG','.gif','.bmp','.svg'];
+  export const carpetaArchivosClientes:string='../../archivos/clientes';
+}
